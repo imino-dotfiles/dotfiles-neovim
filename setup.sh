e821787d52ff7e7782d0a3ec828a5e5b44bdecc9
3u3i <mumiworks@gmail.com>
2020-06-04 18:44:24 +0000

    fixed install script

--- setup.sh
+++ setup.sh
@@ -9,28 +9,28 @@
 
   export NVIM = "$HOME/.config/nvim"
   export DEIN = "$HOME/.cache/dein"
-
+  SCRIPT_DIR=$(cd $(dirname $0); pwd)
 }
 
-function symlink () 
+function symlink()
 { ln -sf $0 $1 }
 
 : "link"
 {
 
-  symlink ./dein/dein.toml $NVIM/dein.toml
-  symlink ./dein/dein_lazy.toml $NVIM/dein_lazy.toml
-  symlink ./dein/denite.vim $NVIM/denite.vim
-  symlink ./dein/lightline.vim $NVIM/lightline.vim
-  symlink ./dein/coc-settings.json $NVIM/coc-setings.json
-  symlink ./nvim/init.vim $NVIM/init.vim
+  symlink $SCRIPT_DIR/dein/dein.toml $NVIM/dein.toml
+  symlink $SCRIPT_DIR/dein/dein_lazy.toml $NVIM/dein_lazy.toml
+  symlink $SCRIPT_DIR/dein/denite.vim $NVIM/denite.vim
+  symlink $SCRIPT_DIR/dein/lightline.vim $NVIM/lightline.vim
+  symlink $SCRIPT_DIR/dein/coc-settings.json $NVIM/coc-setings.json
+  symlink $SCRIPT_DIR/nvim/init.vim $NVIM/init.vim
 
 }           
 
 : "dein setup"
 {
 
-  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+  curl https:/raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 
   # For example, we just use `~/.cache/dein` as installation directory
   sh ./installer.sh ~/.cache/dein
