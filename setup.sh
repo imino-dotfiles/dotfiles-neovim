447fc69d3d2fc1ac6b9bc874d1049cb453458433
iminotech <iminotech@gmail.com>
2020-06-21 16:32:53 +0000

    duh

--- setup.sh
+++ setup.sh
@@ -8,31 +8,28 @@ set -x
 
 : "export" &&
 {
-  export NVIM="$HOME/.config/nvim"
-  export DEIN="$HOME/.cache/dein"
-  SCRIPT_DIR=$(cd $(dirname $0); pwd)
-}
-
-function symlink() {
-  ln -sf $1 $2
+  source ./setup/export.sh
+  # export NVIM="$HOME/.config/nvim"
+  # export DEIN="$HOME/.cache/dein"
+  # SCRIPT_DIR=$(cd $(dirname $0); pwd)
 }
 
 : "link" &&
 {
-
-  symlink $SCRIPT_DIR/dein/dein.toml $NVIM/dein.toml
-  symlink $SCRIPT_DIR/dein/dein_lazy.toml $NVIM/dein_lazy.toml
-  symlink $SCRIPT_DIR/dein/denite.vim $NVIM/denite.vim
-  symlink $SCRIPT_DIR/dein/lightline.vim $NVIM/lightline.vim
-  symlink $SCRIPT_DIR/dein/coc-settings.json $NVIM/coc-setings.json
-  symlink $SCRIPT_DIR/nvim/init.vim $NVIM/init.vim
+  source ./setup/symlink.sh
+  # ln -sf $SCRIPT_DIR/dein/dein.toml $NVIM/dein.toml
+  # ln -sf $SCRIPT_DIR/dein/dein_lazy.toml $NVIM/dein_lazy.toml
+  # ln -sf $SCRIPT_DIR/dein/denite.vim $NVIM/denite.vim
+  # ln -sf $SCRIPT_DIR/dein/lightline.vim $NVIM/lightline.vim
+  # ln -sf $SCRIPT_DIR/dein/coc-settings.json $NVIM/coc-setings.json
+  # ln -sf $SCRIPT_DIR/nvim/init.vim $NVIM/init.vim
 
 }
 
-: "post stuff" &&
+: "postprocess" &&
 {
-
-  nvim -c "UpdateRemotePlugin"
-  nvim -c "UpdateRemotePlugin"
+  source ./setup/post.sh
+  # nvim -c "UpdateRemotePlugin"
+  # nvim -c "UpdateRemotePlugin"
 
 }
