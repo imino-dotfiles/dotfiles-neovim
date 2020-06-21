0f82bdceca8027c71d31732adaf7790eef95da9c
iminotech <iminotech@gmail.com>
2020-06-21 16:44:01 +0000

    duh

--- setup.sh
+++ setup.sh
@@ -4,35 +4,6 @@
 # Requires neovim and curl
 #
 
-set -x
-
-: "export" &&
-{
-  export NVIM="$HOME/.config/nvim"
-  export DEIN="$HOME/.cache/dein"
-  SCRIPT_DIR=$(cd $(dirname $0); pwd)
-}
-
-function symlink() {
-  ln -sf $1 $2
-}
-
-: "link" &&
-{
-
-  symlink $SCRIPT_DIR/dein/dein.toml $NVIM/dein.toml
-  symlink $SCRIPT_DIR/dein/dein_lazy.toml $NVIM/dein_lazy.toml
-  symlink $SCRIPT_DIR/dein/denite.vim $NVIM/denite.vim
-  symlink $SCRIPT_DIR/dein/lightline.vim $NVIM/lightline.vim
-  symlink $SCRIPT_DIR/dein/coc-settings.json $NVIM/coc-setings.json
-  symlink $SCRIPT_DIR/nvim/init.vim $NVIM/init.vim
-
-}
-
-: "post stuff" &&
-{
-
-  nvim -c "UpdateRemotePlugin"
-  nvim -c "UpdateRemotePlugins"
-
-}
+source ./setup/export.sh
+source ./setup/symlink.sh
+source ./setup/post.sh
---
+++ setup/export.sh
@@ -0,0 +1,2 @@
+export NVIM="$HOME/.config/nvim"
+export DEIN="$HOME/.cache/dein"
